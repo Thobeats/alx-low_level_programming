e661203930a6e9e3d1443318ed80cc5bd5f8e8d7
Iyanu Dayisi <tobiy23@gmail.com>
2023-10-05 17:56:59 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry Point
+ *
+ * Returns: Always 0 (Success)
+ *
+ *
+ */
+
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a peice of art,");
+return (0);
+}
