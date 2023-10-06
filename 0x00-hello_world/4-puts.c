3864e25aa8f7d7f77eebfc6f273003a83b586f0a
Iyanu Dayisi <tobiy23@gmail.com>
2023-10-05 17:39:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void){
-puts("\"Programming is like building a multilingual puzzle\n");
-return(0);
+/*
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
 }
