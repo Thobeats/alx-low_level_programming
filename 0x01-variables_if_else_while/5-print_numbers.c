f01c40a6b4b1937b881dc35fe3d5031aa7005eb2
Iyanu Dayisi <tobiy23@gmail.com>
2023-10-06 18:01:09 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
