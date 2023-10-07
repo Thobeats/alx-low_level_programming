17318e3429a8e447e47a4e7d6cc1777bddbb13b0
Iyanu Dayisi <tobiy23@gmail.com>
2023-10-06 17:45:46 -0700

    alphabets 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
