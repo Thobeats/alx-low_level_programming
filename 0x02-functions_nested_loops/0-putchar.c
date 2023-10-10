c9d780783a4e311d7b83e8fc203c28e2e2bf1bac
Iyanu <iyanu.d@novajii.com>
2023-10-10 22:51:03 +0100

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point of the whole Script
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
