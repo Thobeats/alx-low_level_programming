163e83b394c25ea9f82a6cc8afa33f87905d9f1e
Iyanu <iyanu.d@novajii.com>
2023-10-11 04:38:13 +0100

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints the range of numbers from n to 98
+ *
+ * @n: the reference number
+ *
+ * Return: the range of numbers
+ *
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar('0' + n);
+			_putchar(', ');
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar('0' + n);
+			_putchar(', ');
+			n++;
+		}
+	}
+	else
+	{
+		_putchar('0' + n);
+	}
+}
