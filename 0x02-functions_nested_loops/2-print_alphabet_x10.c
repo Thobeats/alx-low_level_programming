33b9684e5eabaf99a40fe6f19abdf359d0190dc3
Iyanu <iyanu.d@novajii.com>
2023-10-11 01:32:45 +0100

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,13 +13,11 @@ void print_alphabet_x10(void)
 	int i, j;
 
 	for (j = 0; j < 10; j++)
-    {
-      for (i = 97; i <= 122; i++)
-      {
-          putchar(i);
-      }
-      putchar('\n');
-    }
-	
-   
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
 }
