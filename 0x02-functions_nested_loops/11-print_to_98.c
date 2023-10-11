f344bb2f496f105ec3064fe595129a587cfaeb76
Iyanu <iyanu.d@novajii.com>
2023-10-11 23:27:40 +0100

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -40,10 +40,22 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			_putchar('0' + (n / 10));
+			if (n >= 100)
+			{
+				_putchar('0' + (n / 100));
+				_putchar('0' + ((n / 10) % 10));
+			}
+			else
+			{
+				_putchar('0' + (n / 10));
+			}
 			_putchar('0' + (n % 10));
-			_putchar(',');
-			_putchar(' ');
+
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			n++;
 		}
 	}
