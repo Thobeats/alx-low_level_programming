a92b6d75b11c0e9a899ef86c3e7e2cff0d3a8c5e
Iyanu <iyanu.d@novajii.com>
2023-10-12 00:36:22 +0100

    print to 98

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - Prints the times table of n
+ *
+ * Return: nothing
+ *
+ */
+
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	if (n <= 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = i * j;
+				if (res > 9)
+				{
+					_putchar('0' + (res / 10));
+				}
+				else if (j > 0)
+				{
+					_putchar(' ');
+				}
+				_putchar('0' + (res % 10));
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
