c6a025354a9789b3cf6d1d5f8e9bfa12c970f04c
Iyanu <iyanu.d@novajii.com>
2023-10-11 03:42:05 +0100

    print last digit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the time of the dady for Jack Bauer
+ *
+ * Return: nothing
+ *
+ *
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	for (int hour = 0; hour < 24; hour++)
+	{
+		if (hour < 10)
+			_putchar('0' + hour + ':');
+		else
+			_putchar(hour + ':');
+
+		for (int minute = 0; minute < 60; minute++)
+		{
+			if (minute < 10)
+			{
+				_putchar('0' + minute);
+			}
+			else
+			{
+				_putchar(minute);
+			}
+		}
+	}
+}
