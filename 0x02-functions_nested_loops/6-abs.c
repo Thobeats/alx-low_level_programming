de154e609dab880c17684b2e45ff3195871687ad
Iyanu <iyanu.d@novajii.com>
2023-10-11 02:39:35 +0100

    print last digit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -16,5 +16,15 @@
 
 int _abs(int n)
 {
-	return (abs(n));
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else{
+		return (n);
+	}
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -18,7 +18,7 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = abs(n % 0);
+	last_digit = abs(n % 10);
 	(int)_putchar(last_digit);
 	return (last_digit);
 }
