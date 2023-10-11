8dcb3ae6b349fdfdae63896cb86bfeb861d42a95
Iyanu <iyanu.d@novajii.com>
2023-10-11 01:05:57 +0100

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Prints out all the alphabet in lowercase
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+	
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; 1 <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
