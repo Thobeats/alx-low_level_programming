2f94f5d6a394d609ab00a0ba7655d6608cc10ad0
Iyanu <iyanu.d@novajii.com>
2023-10-11 02:15:51 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _abs - Verifies the absolute value of an integer
+ *
+ * @n: integer to check
+ *
+ * Return: the absolute value of the integer
+ *
+ *
+ *
+ *
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
