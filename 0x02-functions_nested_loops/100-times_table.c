2f86247b647466ee0a6fd9a39363226155cb7b02
Iyanu <iyanu.d@novajii.com>
2023-10-12 00:49:44 +0100

    print by n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,14 +12,19 @@ void print_times_table(int n)
 {
 	int i, j, res;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				res = i * j;
-				if (res > 9)
+				if (res > 99)
+				{
+					_putchar('0' + (res / 100));
+					_putchar('0' + ((res / 10) % 10));
+				}
+				else if (res > 9 && res <= 99)
 				{
 					_putchar('0' + (res / 10));
 				}
