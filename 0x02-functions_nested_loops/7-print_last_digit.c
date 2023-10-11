3f205a218b3c213c270efbf0677620f6f7919171
Iyanu <iyanu.d@novajii.com>
2023-10-11 02:21:09 +0100

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - Verifies the absolute value of an integer
+ *
+ * @n: integer to check
+ *
+ * Return: the absolute value of the integer
+ *
+ *
+ *
+ *
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
