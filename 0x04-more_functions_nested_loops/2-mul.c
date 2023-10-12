6b7f6c037be021b139a6d251f3419cc5a3f16da6
Iyanu <iyanu.d@novajii.com>
2023-10-12 23:43:16 +0100

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: 0 for success
+ *
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	if ((a >= '0' && a <= '9') && (b >= '0' && b <= '9'))
+	{
+		result = a * b;
+		if (result > 99)
+		{
+			_putchar('0' + (result / 100));
+			_putchar('0' + ((result / 10) % 10);
+		}
+		else if (result > 10)
+		{
+			_putchar('0' + (result / 10));
+		}
+			_putchar('0' + (result % 10));
+	}
+
+	return (0);
+}
