9530a3593594d8364f21d2ce1e6080a0ce6e56d4
Iyanu <iyanu.d@novajii.com>
2023-10-13 01:10:36 +0100

    multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -14,33 +14,7 @@
 int mul(int a, int b)
 {
 	int result;
-	/**
-	 * char buffer[10];
-	 * int i = 0;
-	 */
 
 	result = a * b;
-	/**if (result < 0)
-	{
-		result = -result;
-	}
-
-	else
-	{
-		while (result > 0)
-		{
-			buffer[i] = '0' + (result % 10);
-			result /= 10;
-			i++;
-		}
-
-		while (i > 0)
-		{
-			i--;
-			_putchar(buffer[i]);
-		}
-
-	}**/
-
 	return result;
 }
