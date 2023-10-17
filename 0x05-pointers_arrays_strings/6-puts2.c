3ca425eb0102641c5f4a9e7de82928a9b80b003f
tobi <tobiy23@gmail.com>
2023-10-16 18:38:23 -0700

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string to stdout
+ *
+ * @str: the string
+ *
+ * Return: void
+ *
+ */
+
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+
+	putchar('\n');
+}
