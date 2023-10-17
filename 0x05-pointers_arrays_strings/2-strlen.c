3458628c745d2dcfd6f16daab390245d0d069909
tobi <tobiy23@gmail.com>
2023-10-16 17:10:17 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _strlen - swaps the values of two integers
+ *
+ * @s: the string
+ *
+ * Return: the length of the string
+ *
+ */
+
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(*s);
+
+	return (length);
+}
