57a3e8b33493395094052537cb73e34a1849bd3f
tobi <tobiy23@gmail.com>
2023-10-16 19:16:15 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,16 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i <= n)
+	while (i < n)
 	{
-		printf("%d,", a[i]);
+		if (n - i > 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
 		i++;
 	}
 
