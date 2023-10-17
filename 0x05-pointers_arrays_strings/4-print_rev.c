ec40d5c575d8b6d9efefe83f912290978cd964b6
tobi <tobiy23@gmail.com>
2023-10-16 17:41:13 -0700

    print rev string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ *
+ * @s: the string
+ *
+ * Return: void
+ *
+ */
+
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+
+	putchar('\n');
+
+	return (length);
+}
