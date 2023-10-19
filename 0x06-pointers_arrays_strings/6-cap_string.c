591de4068d0cb9c3d1a3c2a01d9c339274415057
tobi <tobiy23@gmail.com>
2023-10-19 15:10:30 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize every word of a string
+ *
+ * @str: char array
+ *
+ * Return: uppercase character
+ */
+
+char *cap_string(char *str)
+{
+	char seperators[] = " \t\n,.;!?\"(){}";
+	bool isSeperator;
+	int i;
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		for (i = 0; seperators[i] != '\0'; i++)
+		{
+			if (*ptr == seperators[i])
+			{
+				isSeperator = 1;
+			}
+			else
+			{
+				isSeperator  = 0;
+			}
+
+			if (isSeperator)
+			{
+				ptr++;
+				if (*ptr >= 'a' && *ptr <= 'z')
+				{
+					*ptr = *ptr - 32;
+				}
+			}
+
+		}
+
+		ptr++;
+	}
+
+
+	return (str);
+}
