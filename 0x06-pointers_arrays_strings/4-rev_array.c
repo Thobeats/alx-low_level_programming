a29d933f186fa6d1a53d19d6c9066bef26fb6e82
tobi <tobiy23@gmail.com>
2023-10-19 13:22:44 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the order of the elements in an array
+ *
+ * @a: the array
+ * @n: the length of the array
+ *
+ * Return: the reversed array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	while (n > 0 && i < 0)
+	{
+		*a[i] = *a[n];
+
+		n--;
+		i++;
+	}
+}
