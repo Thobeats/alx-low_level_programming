3cdaab0f549eaf9485a68becf974c6fdd3880210
tobi <tobiy23@gmail.com>
2023-10-20 21:18:12 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string with the rot13 format
+ *
+ * @str: char array
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *str)
+{
+	char lowercase[] = "abcdefghijklm";
+	char uppercase[] = "ABCDEFGHIJKLM";
+	char code[] = "43071";
+	int i;
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		for (i = 0; lowercase[i] != '\0'; i++)
+		{
+			if (*ptr == lowercase[i] || *ptr == uppercase[i])
+			{
+				*ptr = *ptr + 13;
+			}else{
+				*ptr = *ptr - 13;
+			}
+		}
+
+		ptr++;
+	}
+
+
+	return (str);
+}
