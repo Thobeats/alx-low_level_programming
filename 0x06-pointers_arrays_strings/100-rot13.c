df650a82caef43a8b18cd98dd28f49829f21d87c
tobi <tobiy23@gmail.com>
2023-10-21 03:05:37 -0700

    rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -20,7 +20,9 @@ char *rot13(char *str)
 		for (i = 0; lowercase[i] != '\0'; i++)
 		{
 			if (*ptr == lowercase[i] || *ptr == uppercase[i])
+			{
 				*ptr = *ptr + 13;
+			}
 			else
 			{
 				*ptr = *ptr - 13;
