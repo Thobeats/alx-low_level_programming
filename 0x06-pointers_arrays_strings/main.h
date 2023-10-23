6e44651dfd39c88c8b602e9689983995a02a5142
tobi <tobiy23@gmail.com>
2023-10-23 15:34:27 -0700

    ; update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *rot13(char *);
 #endif /* MAIN_H */
