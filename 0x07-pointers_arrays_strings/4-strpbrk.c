16531fe9491421d1f5466b88f36ef8b7aa0d886a
tobi <tobiy23@gmail.com>
2023-10-23 18:18:26 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -17,9 +17,9 @@ char *_strpbrk(char *s, char *accept)
 	while (*s != '\0')
 	{
 		*a = accept;
-		while (*accept != '\0')
+		while (*a != '\0')
 		{
-			if (*s == *accept)
+			if (*s == *a)
 			{
 				return (s);
 			}
