7fe9fa68f20cb019b9400e5d8b42a41a4132c17c
tobi <tobiy23@gmail.com>
2023-10-24 15:22:39 -0700

    set string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 #endif /* MAIN_H */
