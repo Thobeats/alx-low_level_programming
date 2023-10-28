51f7c0001431777fb4a4022449245170872972fa
tobi <tobiy23@gmail.com>
2023-10-27 17:06:19 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments
+ *
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0 for success
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+
+}
