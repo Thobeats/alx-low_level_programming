151c499f5f29923ab9de46d93cb77c248e3153da
tobi <tobiy23@gmail.com>
2023-10-27 18:00:39 -0700

    print addition of args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main - Prints the addition of two arguments
+ *
+ * @argc: argument count
+ * @argv: an array of the argumentsi
+ *
+ * Return: 0 for success
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+
+	if (argc == 1)
+		printf("0\n");
+	else if (argc > 1){
+
+		for (a = 1; a < argc; a++)
+		{
+			if (isdigit(argv[a]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			b += atoi(argv[a]);
+		}
+
+		printf("%d\n", b);
+	}
+
+	return (0);
+
+}
