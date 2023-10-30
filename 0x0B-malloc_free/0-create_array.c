39e63c4ff0b8e0520ad626cdbac2e58ff2070c03
tobi <tobiy23@gmail.com>
2023-10-30 05:55:26 -0700

    update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * create_array - Creates an array of chars and initializes it.
+ *
+ * @size: Size of the array
+ * @c: the character
+ *
+ * Return: pointer to the array or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int _putchar(char c);
+int calc_change(int *change, int cents);
+int _myname(int argc, char *argv[]);
+#endif /* MAIN_H */
