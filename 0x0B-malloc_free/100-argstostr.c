5ab4df74ad4bbabfff3d9772352344d0ed9319e6
Iyanu <tobiy23@gmail.com>
2023-11-01 02:46:33 +0100

    arg to string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * argstostr - Concatenates all the args of the program
+ *
+ * @ac: the length of the args
+ * @av: the arguments
+ *
+ * Return: concatenated string, NULL if ac is 0 or av is NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *avmalloc;
+	int i;
+	int j;
+	int length;
+	int k;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			continue;
+		}
+
+		length += j + 1;
+	}
+
+	*avmalloc = (char *)malloc((sizeof(char) * length) + 1);
+
+	if (avmalloc == NULL)
+		return (NULL);
+
+	i = 0;
+	k = 0;
+	while (i < ac)
+	{
+		for (j = 0; av[i][j]; j++, k++)
+		{
+			avmalloc[k] = av[i][j];
+		}
+
+		avmalloc[k] = '\n';
+
+		if (i == (ac - 1))
+		{
+			avmalloc[k + 1] = '\0';
+		}
+
+		k++;
+		i++;
+	}
+
+	return (avmalloc);
+}
