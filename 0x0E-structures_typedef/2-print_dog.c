a464a1891f220b9af33d6246dce371a5622b15e7
tobi <tobiy23@gmail.com>
2023-11-06 06:55:19 -0800

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#define NULL 0;
+
+/**
+ * print_dog - Prints the details of the dog struct
+ *
+ * @d: The dog structure address
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+
+}
