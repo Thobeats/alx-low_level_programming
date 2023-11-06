94b8e285df89e54964a7b17b26eb07470c15e78f
Iyanu <tobiy23@gmail.com>
2023-11-07 00:23:44 +0100

    base file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the base file
+ * 
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+    printf("%s\n", __BASE_FILE__);
+    return (0);
+}
