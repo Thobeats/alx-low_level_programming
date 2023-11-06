81267d7d3601efea7a22a769d0c663818b1ed3e2
tobi <tobiy23@gmail.com>
2023-11-06 06:06:00 -0800

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,23 @@
+#ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - Creates a dog structure
+ *
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This structure contains the profile of a dog blueprint.
+ *
+ *
+ */
 
 struct  dog
 {
-    char * name;
-    float age;
-    char * owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif
