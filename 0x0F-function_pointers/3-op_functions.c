6ba35c2f85daafb0179f86e60227005d9f3aa7c4
Iyanu Dayisi <tobiy23@gmail.com>
2023-11-09 05:04:10 +0100

    calc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+
+/**
+ * op_add - Returns the sum of two args
+ *
+ * @a: arg 1
+ * @b: arg 2
+ *
+ * Return: sum of args
+ *
+ */
+
+int op_add(int a, int b)
+{
+		return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference between two args
+ *
+ * @a: arg 1
+ * @b: arg 2
+ *
+ * Return: The difference between the args
+ *
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of two args
+ *
+ * @a: arg 1
+ * @b: arg 2
+ *
+ *
+ * Return: The product of the two args
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the division between two integers
+ *
+ * @a: arg 1
+ * @b: arg 2
+ *
+ * Returns: The division between the two args
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the modulus between two integers
+ *
+ * @a: arg 1
+ * @b: arg 2
+ *
+ * Return: The modulus between the two args
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
