45d3f3596b43c5a881efb4487c7974b78b2e2471
Iyanu Dayisi <tobiy23@gmail.com>
2023-11-09 04:02:27 +0100

    Array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,11 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL)
-		return;
-	else if (action == NULL)
-		return;
-	else
+	if (array != NULL && action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
