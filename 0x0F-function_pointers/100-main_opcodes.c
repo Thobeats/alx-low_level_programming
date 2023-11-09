a3796266ce690d96e101b9e1d55cad27ac2575f1
Iyanu Dayisi <tobiy23@gmail.com>
2023-11-09 05:09:47 +0100

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of program arguments
+ *
+ * @argv: array of input arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	x = atoi(argv[1]);
+	if (x < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return (0);
+}
