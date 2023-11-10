18a659526cd1db60e1495cafb4f9f54bb9c4a119
Iyanu Dayisi <tobiy23@gmail.com>
2023-11-10 01:45:35 +0100

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,9 +25,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
+		s = va_arg(args, char *);
 		if (i < (n - 1))
 		{
-			s = va_arg(args, char *);
 			if (s == NULL)
 				s = "(nil)";
 
