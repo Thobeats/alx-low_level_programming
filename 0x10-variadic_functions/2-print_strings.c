da55e9556da6049972ffe4c370718204efdc042b
Iyanu Dayisi <tobiy23@gmail.com>
2023-11-10 01:50:26 +0100

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,11 +26,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char *);
-		if (i != (n - 1))
+		if (i < (n - 1))
 		{
 			if (s == NULL)
 				s = "(nil)";
-
 			printf("%s", s);
 			if (separator != NULL)
 				printf("%s", separator);
@@ -38,6 +37,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s\n", s);
 	}
-
 	va_end(args);
 }
