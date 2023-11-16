91669a212c3a9eae928a808669027cc8fcf28fd9
Thobeats <tobiy23@gmail.com>
2023-11-16 22:12:28 +0100

    update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints the values the linked lists
+ *
+ * @h: a pointer to the header of the list
+ *
+ * Return: The size of the list 
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+	const list_t *current;
+	char *output;
+
+	count = 0;
+
+	current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			output = "[0] (nil)";
+			while (*output != '\0')
+			{
+				_putchar(*output);
+				output++;
+			}
+			_putchar('\n');
+		}
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
+		
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
+
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LIST_H
+#include <stdio.h>
+/**
+ * struct list_s - The structure of the list s
+ *
+ * @str: the string property of the structure
+ * @len: the size of the string property
+ * @next: the pointer to the next list, points to NULL if it is the last
+ *
+ * Description: The structure holds three properties
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
