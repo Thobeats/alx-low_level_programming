3565caa4007d65bfa4744a741261950ef831c2f4
Thobeats <tobiy23@gmail.com>
2023-11-20 23:55:30 +0100

    free the list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free the list
+ *
+ * @head: The head of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return;
+	while ((*head) != NULL)
+	{
+		current = (*head);
+		(*head) = (*head)->next;
+		free(current);
+	}
+	head = NULL;
+}
