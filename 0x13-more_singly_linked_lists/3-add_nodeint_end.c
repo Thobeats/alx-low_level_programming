b74343813bff90210ea336bea4d4dd652d771334
Thobeats <tobiy23@gmail.com>
2023-11-20 22:39:48 +0100

    add node at the end of the list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,11 +21,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	current = (*head);
 	newList->n = n;
-
 	if (current != NULL)
 	{
 		newList->next = (*head);
 		(*head) = newList;
+
+		return ((*head));
 	}
 
 	while (current != NULL)
