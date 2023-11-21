4ac20ff73934ae60c091513ef036bdec37308e79
Thobeats <tobiy23@gmail.com>
2023-11-21 21:20:38 +0100

    add node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newList->n = n;
 		newList->next = NULL;
 		(*head) = newList;
-		return ((*head));
+		return (newList);
 	}
 
 	while (current->next != NULL)
