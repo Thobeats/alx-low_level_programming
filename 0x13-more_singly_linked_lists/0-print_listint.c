3a8daf43f27a91a4589d354d39aef8d14c36f308
Thobeats <tobiy23@gmail.com>
2023-11-22 01:13:31 +0100

    delete node

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -16,10 +16,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n != '\0')
-		{
-			printf("%d\n", h->n);
-		}
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,27 +13,41 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current1;
 	listint_t *current2;
+	listint_t *node;
 	unsigned int count;
 
 	if (head == NULL)
-		return (-1);
-	count = 0;
-	current1 = (*head);
-	current2 = current1->next;
-	while (current2->next != NULL)
 	{
-		if (count == (idx - 1))
+		return (-1);
+	}
+
+	current1 = (*head);
+	count = 0;
+
+	while (current1 != NULL)
+	{
+		if (count == (index - 1))
+			current2 = current1;
+
+		if (index == count)
 		{
-			current2 = current1->next;
-			free(current1->next);
-			current1->next = current2->next;
-			return (1);
+			node = current1;
+			current1 = current1->next;
+			break;
 		}
-		else
-			current1 = current2;
-			current2 = current1->next;
+
+		current1 = current1->next;
 		count++;
 	}
 
-	return (-1);
+	if (current1 == NULL)
+		return (-1);
+
+	if (index == 0)
+		(*head) = current1;
+	else
+		current2->next = current1;
+
+	free(node);
+	return (1);
 }
