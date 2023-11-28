4f653b8302734cb53dbc2c46c53bc39b793c97b0
Thobeats <tobiy23@gmail.com>
2023-11-28 01:50:11 +0100

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,8 +22,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		return (1);
-
-	while (*text_content)
+	text_count = 0;
+	while (text_content[text_count])
 		text_count++;
 	write(filecreate, text_content, text_count);
 	if (close(filecreate) < 0)
