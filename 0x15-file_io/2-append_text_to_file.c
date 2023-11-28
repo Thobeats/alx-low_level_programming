c1e7d165340780763d9c402b6caae3b4fa2da1ca
Thobeats <tobiy23@gmail.com>
2023-11-28 01:42:32 +0100

    write file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	filecreate = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	filecreate = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
 	if (filecreate == -1)
 		return (-1);
 
