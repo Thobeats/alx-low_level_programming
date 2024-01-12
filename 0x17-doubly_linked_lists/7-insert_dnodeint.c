ab9134821bb9d4e997caaef8a44ce5d25c1fb2ae
Iyanu <tobiy23@gmail.com>
2024-01-12 04:41:26 +0100

    insert node at int

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode;
+	dlistint_t *i;
+	dlistint_t *j;
+	unsigned int count;
+
+	if (h == NULL && idx > 0)
+		return (NULL);
+	newNode = malloc(sizeof(*newNode));
+	if (!newNode)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = NULL;
+	newNode->prev = NULL;
+
+	i = *h;
+	count = 0;
+	while (i != NULL)
+	{
+		count++;
+		if (count == idx)
+		{
+			j = i->next;
+			i->next = newNode;
+			newNode->prev = i;
+			j->prev = newNode;
+			newNode->next = j;
+			
+			return (*h);
+		}
+		i = i->next;
+	}
+
+	return (NULL);
+}
