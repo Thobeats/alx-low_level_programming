ef1475f39136390b84271faada631ae88499f178
Iyanu <tobiy23@gmail.com>
2024-02-03 02:57:41 +0100

    update hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,13 +18,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	hashTable->size = size;
-	hashTable->array = malloc(sizeof(hash_table_t) * size);
+	hashTable->array = malloc(sizeof(hash_table_t*) * size);
 	if (hashTable->array == NULL)
 	{
 		free(hashTable);
 		return (NULL);
 	}
-	for (count = 0; count <= size; count++)
+	for (count = 0; count < size; count++)
 		hashTable->array[count] = NULL;
 	return (hashTable);
 }
