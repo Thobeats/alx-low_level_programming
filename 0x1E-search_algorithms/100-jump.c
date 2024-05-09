f0d0765932da628640a532bfdc0be45e1eda7414
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-09 17:06:53 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -41,8 +41,22 @@ int jump_search(int *array, size_t size, int value)
 			}
 			break;
 		}
-	}
 
+		if (high > (int)size)
+		{
+			printf("Value found between indexes [%d] and [%d]\n", low, high);
+			while (low <= high)
+			{
+				printf("Value checked array[%d] = [%d]\n", low, array[low]);
+				if (array[low] != value)
+				{
+					return (-1);
+				}
+				low++;
+			}
+			break;
+		}
+	}
 	return (-1);
 }
 
