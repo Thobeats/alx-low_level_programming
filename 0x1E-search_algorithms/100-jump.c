db1305884d611bd37dd907a41d8531e714898fd8
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-10 10:49:07 +0100

    jump search algo

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,5 @@
 #include "search_algos.h"
 #include <math.h>
-
 /**
  * jump_search - function that searches for a value in a sorted array of
  *			integers using the Binary search algorithm
@@ -12,16 +11,23 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int jump = sqrt(size), low = 0, high = low;
+	int jump = sqrt(size);
+	int low = 0;
+	int high = low;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	while (high <= ((int)size + jump))
+	while (high <= (int)size)
 	{
-		printf("Value checked array[%d] = [%d]\n", high, array[high]);
+		if (value > array[high])
+		{
+			printf("Value checked array[%d] = [%d]\n", high, array[high]);
 
-		if ((value > array[low] && value < array[high]))
+			low = high;
+			high = low + jump;
+		}
+		else
 		{
 			printf("Value found between indexes [%d] and [%d]\n", low, high);
 			while (low <= high)
@@ -35,20 +41,6 @@ int jump_search(int *array, size_t size, int value)
 			}
 			break;
 		}
-
-		if (high > (int)size)
-		{
-			printf("Value found between indexes [%d] and [%d]\n", low, high);
-			while (low <= (int)size - 1)
-			{
-				printf("Value checked array[%d] = [%d]\n", low, array[low]);
-				low++;
-			}
-		}
-
-
-		low = high;
-		high = low + jump;
 	}
 	return (-1);
 }
